60d0434f6bf4be175164dbb09574e60ff510572a
Joseph Banks <banksjh@gmail.com>
2015-11-01 19:49:16 -0600

    Added Picasso library

--- app/src/main/java/com/squattyapple/android/popularmovies/MovieListActivity.java
+++ app/src/main/java/com/squattyapple/android/popularmovies/MovieListActivity.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
-
 public class MovieListActivity extends AppCompatActivity {
 
     @Override
@@ -17,15 +16,6 @@ public class MovieListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_movie_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
     @Override
