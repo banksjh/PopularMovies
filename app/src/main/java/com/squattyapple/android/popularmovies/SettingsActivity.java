e41f52524b6a5ec0cfe118c32f40cf5390e9385d
Joseph Banks <banksjh@gmail.com>
2015-11-08 08:03:46 -0600

    Removed unused menu option

--- app/src/main/java/com/squattyapple/android/popularmovies/SettingsActivity.java
+++ app/src/main/java/com/squattyapple/android/popularmovies/SettingsActivity.java
@@ -135,6 +135,7 @@ public class SettingsActivity extends PreferenceActivity {
         @Override
         public void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
+
             addPreferencesFromResource(R.xml.pref_general);
             setHasOptionsMenu(true);
 
